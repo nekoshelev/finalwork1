ef8a207391d167f03cdcd1c98a938687f1d8e9fa
«Nikolay <>
2022-08-24 11:05:54 +0600

    formating

--- Program.cs
+++ Program.cs
@@ -5,22 +5,35 @@
 
 string[] array = new string[] { "geekbrains", "gb", "skill", "red", "123" };
 
-void methodArray (string [] array){
-     string[] arr = new string[array.Length];
-     for (int i=0; i<array.Length; i++){      
-        if(array[i].Length<=3){
-        arr[i]=array[i];
-        Console.WriteLine(arr[i]); 
+void methodArray(string[] array)
+{
+    Console.Write("[ ");
+    string[] arr = new string[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            arr[i] = array[i];
+            Console.Write($"{arr[i]} ");
         }
-     }
-}
-
-void Print(string [] Array){
-    for (int i=0; i<Array.Length; i++){
-        Console.WriteLine(Array[i]);
     }
-    Console.WriteLine();
+    Console.Write("]");
 }
 
+void Print(string[] Array)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < Array.Length; i++)
+    {
+        Console.Write(Array[i]);
+        Console.Write(" ");
+    }
+    Console.Write("]");
+}
+Console.WriteLine();
+Console.WriteLine("Заданный массив: ");
 Print(array);
+
+Console.WriteLine();
+Console.WriteLine("Результат: ");
 methodArray(array);
