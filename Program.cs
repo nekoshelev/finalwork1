40f0fa126b53e9b6ba74f340a2515ee4616bbf25
«Nikolay <>
2022-08-24 10:47:26 +0600

    add method

--- Program.cs
+++ Program.cs
@@ -4,3 +4,13 @@
 // При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массива
 
 string[] array = new string[] { "geekbrains", "gb", "skill", "red", "123" };
+
+void methodArray (string [] array){
+     string[] arr = new string[array.Length];
+     for (int i=0; i<array.Length; i++){      
+        if(array[i].Length<=3){
+        arr[i]=array[i];
+        Console.WriteLine(arr[i]); 
+        }
+     }
+}
